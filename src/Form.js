d957ad7397a6dd55ce1aca0dce5f397ab944a88f
Pham Huu Hien <phhien203@gmail.com>
2022-05-22 10:24:15 +0700

    Binding state object into form controls

--- src/Form.js
+++ src/Form.js
@@ -2,19 +2,50 @@ import React, { useState } from "react";
 import "./Form.css";
 
 const Form = () => {
-  const [firstName, setFirstName] = useState("");
+  const initialState = {
+    firstName: "",
+    lastName: "",
+  };
+  const [formState, setFormState] = useState(initialState);
+
+  const updateFormState = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const resetFormState = () => {
+    setFormState(initialState);
+  };
 
   console.log("main render");
   return (
-    <form>
+    <form autoComplete="off" onSubmit={onFormSubmit}>
       <label htmlFor="firstName">First name</label>
       <input
         id="firstName"
+        name="firstName"
         type="text"
-        value={firstName}
-        onChange={(e) => setFirstName(e.target.value)}
+        value={formState.firstName}
+        onChange={updateFormState}
       />
-      <span>{firstName}</span>
+      <label htmlFor="lastName">Last name</label>
+      <input
+        id="lastName"
+        name="lastName"
+        type="text"
+        value={formState.lastName}
+        onChange={updateFormState}
+      />
+      <button type="submit">Save</button>
+      <button type="button" onClick={resetFormState}>
+        Clear Form
+      </button>
     </form>
   );
 };
